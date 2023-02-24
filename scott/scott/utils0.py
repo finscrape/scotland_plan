148989e80501674b4ea51b4f3bb04a4a6ccbf782
finscrape <78282593+finscrape@users.noreply.github.com>
2023-02-24 17:33:05 +0100

    Update utils0.py

--- scott/scott/utils0.py
+++ scott/scott/utils0.py
@@ -154,27 +154,12 @@ def cookies_parsen():
     
     return parsed_cookie
 
-# def params():
-#     param = 'searchQueryState=%7B%22pagination%22%3A%7B%22currentPage%22%3A2%7D%2C%22usersSearchTerm%22%3A%22New%20York%2C%20NY%22%2C%22mapBounds%22%3A%7B%22west%22%3A-74.4009301328125%2C%22east%22%3A-73.5549828671875%2C%22south%22%3A40.45035852051812%2C%22north%22%3A40.96047101556829%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A6181%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Afalse%2C%22filterState%22%3A%7B%22price%22%3A%7B%22min%22%3A100000%2C%22max%22%3A500000%7D%2C%22monthlyPayment%22%3A%7B%22min%22%3A323%2C%22max%22%3A1617%7D%2C%22isAllHomes%22%3A%7B%22value%22%3Atrue%7D%7D%2C%22isListVisible%22%3Atrue%7D&wants={%22cat1%22:[%22listResults%22]}&requestId=5'
-#     cookie = SimpleCookie()
-#     cookie.load(param)
-#     parsed_cookie = {}
-#     for key,morsel in cookie.items():
-#         parsed_cookie[key] = morsel.value
-
-#     encoded_cookie = urlencode(parsed_cookie)
-#     return encoded_cookie
-
+def cookies_parsep():
+    cookie_string = 'JSESSIONID=ovsJeH5qqAU6fiBn2CzYOhGK34Hx6bC2Ij_6ijVE.pawebhst49a; citrix_ns_id_.midlothian.gov.uk_%2F_wat=AAAAAAVPHZ1V1oujBbWRjuUxntk4_sMwhmw8pa9hZVFHCvdhgcJKkBSV6KEgivrrDc2X6SEbqnxPb-U9zEzs4Hv1O2MY#dcLYf+bC79jQY1XncvhA9CvOlKsA&'
+    cookie = SimpleCookie()
+    cookie.load(cookie_string)
+    parsed_cookie = {}
+    for key,morsel in cookie.items():
+        parsed_cookie[key] = morsel.value
     
-# def parse_new_url(url, page_number):
-#     url_parsed = urlparse(url)
-#     query_string = parse_qs(url_parsed.query)
-#     search_query_state = json.loads(query_string.get('searchQueryState')[0])
-#     search_query_state['pagination'] = {"currentPage": page_number}
-#     query_string.get('searchQueryState')[0] = search_query_state
-#     encoded_qs = urlencode(query_string, doseq=1)
-#     new_url = f"https://www.zillow.com/search/GetSearchPageState.htm?{encoded_qs}"
-#     return new_url
-
-
-    
+    return parsed_cookie
